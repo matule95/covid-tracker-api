fa0e8cb58fbab3cb07c4bcee13baf154ba1fbe4f
Matule <clayton.matule@hotmail.com>
2021-09-05 17:55:36 +0200

    feat: added cors support to api

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const logger = require("./utils")
+const logger = require("./utils");
 module.exports = {
-    initializeServer: () => {
-        logger.dispatch("Initializing Express Server");
-        const express = require("express");
-        const app = express();
-        const port = 8088;
-        const routes = require("./routes/");
-        routes(app);
-        app.use((req, res) => {
-            res.status(404).send({ url: `${req.originalUrl} doesn't exist` });
-        });
-        app.listen(port);
-        logger.dispatch(`Express Server running on port:${port}`);
-    },
+  initializeServer: () => {
+    logger.dispatch("Initializing Express Server");
+    const express = require("express");
+    const cors = require("cors");
+    const app = express();
+    const port = 8088;
+    const routes = require("./routes/");
+    routes(app);
+    app.use(
+      cors({
+        origin: [
+          "https://covid19.co.mz",
+          "http://covid19.co.mz",
+          "https://covidmoz.netlify.app/",
+        ],
+        methods: ["GET"],
+      })
+    );
+    app.use((req, res) => {
+      res.status(404).send({ url: `${req.originalUrl} doesn't exist` });
+    });
+    app.listen(port);
+    logger.dispatch(`Express Server running on port:${port}`);
+  },
 };
